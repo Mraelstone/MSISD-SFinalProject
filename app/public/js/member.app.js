f8c4aaf5e2386bfc7c9c53f5985408f3af22ebb6
Nistha Gautam <nish271996@gmail.com>
2019-10-17 01:47:17 -0400

    html changes minor

--- app/public/js/member.app.js
+++ app/public/js/member.app.js
@@ -10,6 +10,22 @@ var memberRecordsApp = new Vue({
       .then(response => response.json())
       .then(json => { memberRecordsApp.members = json })
     },
+    handleSubmit(event) {
+      fetch('api/members/post.php', {
+        method:'POST',
+        body: JSON.stringify(this.recordMember),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => { memberRecordsApp.Members.push(json[0]) })
+      .catch( err => {
+        console.error('RECORD POST ERROR:');
+        console.error(err);
+      })
+      this.handleReset();
+    },
     handleReset() {
       this.recordMember = {
         firstName: '',
