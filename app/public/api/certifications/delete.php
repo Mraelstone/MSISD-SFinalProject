f473ba0e82631e0364c319d4b6380e77bf0803fa
Nistha Gautam <nish271996@gmail.com>
2019-10-27 02:26:03 -0400

    Add/Update/Delete Certifications and Assigning Certifications to Members

--- app/public/api/certifications/delete.php
+++ app/public/api/certifications/delete.php
@@ -1,21 +1,19 @@
 <?php
+use Ramsey\Uuid\Uuid;
 // Step 0: Validate Data
 
 // Step 1: Get a datase connection from our help class
 $db = DbConnection::getConnection();
-
 // Step 2: Prepare & run the query
 $stmt = $db->prepare(
-  'INSERT INTO PatientVisit
-  (patientGuid, visitDescription, Priority)
-  VALUES(?,?,?)'
+  'DELETE FROM Certification WHERE certificationGuid=?'
 );
+
+
 
 //How to prevent SQL injections:
 $stmt->execute([
-  $_POST['patientGuid'],
-  $_POST['visitDescription'],
-  $_POST['priority']
+  $_POST['certificationGuid']
 ]);
 
 //Global Variables: can introduce bad security practices (SQL injection ex.)
@@ -30,8 +28,5 @@ $stmt->execute([
 // Step 4: Output
 header('HTTP/1.1 303 See Other');
 //303 status code says redirect with a Get
-header('Location: ../waiting/');
+header('Location: ../certifications/');
 //tells you where the other place is you're sending it to
-// Step 4: Output
-header('HTTP/1.1 303 See Other');
-header('Location: ../waiting/');
