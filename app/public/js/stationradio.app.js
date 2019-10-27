1fd2da11f915c6ca3235a6e38f5c978b8707c491
Claire Majewski <cmajewsk@iu.edu>
2019-10-27 14:04:04 -0400

    Fixed report for filtering members by radio no. and station no.

--- app/public/js/stationradio.app.js
+++ app/public/js/stationradio.app.js
@@ -1,45 +1,66 @@
 var stationradio = new Vue({
-  el: '#stationradio',
-  data: {
-    radios: [],
-    stations: [],
-    radiostation:{},
-    resultForReport: [],
-    members:[]
-  },
-  methods: {
-    sendSelected() {
-      fetch('api/members/stationradio.php?radioNumber='+this.radiostation.radioNumber+'&stationNumber='+this.radiostation.stationNumber)
-        .then(response => response.json())
-        .then(json => {
-          stationradio.resultForReport = json;
-          //this.handleReset();
-          console.log(stationradio.resultForReport); })
-    },
-    getRadios() {
-      fetch('api/members/radiolist.php')
-    .then( response => response.json() )
-    .then( json => {stationradio.radios = json;
-          console.log(this.radios);
-        })
-    },
-    getStations() {
-      fetch('api/members/stationlist.php')
-    .then( response => response.json() )
-    .then( json => {stationradio.stations = json;
-      console.log(this.stations);})
-  },
-  handleReset() {
-    this.radiostation = {
-      radioNumber:'',
-      stationNumber:''
-    }
-  }
-  },
-  created() {
-    this.getRadios();
-    this.getStations();
-    this.handleReset();
-    //this.fetchStationRadio();
-  }
+  el: '#stationradio',
+  data: {
+    radios: [],
+    stations: [],
+    radiostation:{},
+    resultForReport: [],
+    members:[],
+    member:{},
+    filter:{
+      radioNumber:'',
+      stationNumber:''
+    }
+  },
+  methods: {
+    fetchMembers() {
+      fetch('api/members/index.php')
+      .then(response => response.json())
+      .then(json => { stationradio.members = json })
+    },
+    getRadios() {
+      fetch('api/members/radiolist.php')
+    .then( response => response.json() )
+    .then( json => {stationradio.radios = json;
+          console.log(this.radios);
+        })
+    },
+    getStations() {
+      fetch('api/members/stationlist.php')
+    .then( response => response.json() )
+    .then( json => {stationradio.stations = json;
+      console.log(this.stations);})
+  },
+
+  handleMemberReset() {
+    this.recordMember = {
+      firstName: '',
+      lastName: '',
+      sexAtBirth: '',
+      email:'',
+      dob:'',
+      addrStreet:'',
+      addrCity:'',
+      addrState:'',
+      addrZipcode:'',
+      workPhone:'',
+      mobilePhone: '',
+      radioNumber:'',
+      startDate:'',
+      stationNumber:'',
+      isActive:'',
+      memberPosition:''
+    }
+  },
+    handleRowClick(member){
+      stationradio.member = member;
+  }
+  },
+  created() {
+    this.fetchMembers();
+    this.getRadios();
+    this.getStations();
+    this.handleMemberReset();
+    //this.fetchStationRadio();
+  }
 })
